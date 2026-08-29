d40ee2010a1542297276219e5698d50f1041f11f
Rafi Zaki <rafizaki745@gmail.com>
2026-08-29 23:51:15 +0700

    chore: remove legacy Blade frontend views and controller, streamline backend as pure API server

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/denah', [HomeController::class, 'denah'])->name('denah');
-Route::post('/chatbot/send', [\App\Http\Controllers\Api\Public\ChatbotController::class, 'send'])->name('chatbot.send');
-
-https://127.0.0.1:50142/static/artifacts/4b4156fe-b62f-474e-a9a2-737ae5e556cc/.user_uploaded/media_1787985147215.png?csrf=44398169-70a8-4301-99dd-f1318f769d43
+Route::get('/', function () {
+    return response()->json([
+        'app' => 'SMK Negeri 2 Kota Mojokerto - School Portal API',
+        'status' => 'online',
+        'version' => '1.0.0',
+        'frontend' => 'Next.js (frontend-next)',
+    ]);
+});
